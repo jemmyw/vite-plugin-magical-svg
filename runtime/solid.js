ec2c2b1074e27d4b05730680d83f04720acabe8b
Cynthia <cynthia@cynthia.dev>
2024-07-12 12:33:36 +0200

    fix: proper solid import

--- runtime/solid.js
+++ runtime/solid.js
@@ -1,4 +1,4 @@
-import { template, spread } from "../test.nogit/node_modules/solid-js/web/dist/web";
+import { template, spread } from "solid-js/web";
 
 export var createSvg = /*@__NO_SIDE_EFFECTS__*/ (viewBox, width, height, symbol) => {
 	let tmpl = template(`<svg ${viewBox?`viewBox="${viewBox}"`:''} ${width?`width="${width}"`:''} ${height?`height="${height}"`:''}><use href="${symbol}">`);
